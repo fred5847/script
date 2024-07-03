adad402e89f150c9b95bc168ef4f50424fb1970a
fedekat <frederik5847@gmail.com>
2024-07-03 09:10:10 +0200

    initial scripts

---
+++ startChromium.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DISPLAY=:0 chromium-browser https://cloud.humio.com/shared/dashboards?token=jmWPS5uiagjHoeT96GB84pO6\&theme=dark --start-fullscreen --incognito&
+DISPLAY=:0 xdotool mousemove 0 0
